4d1ed8571e7d26e04b6eeafbdb49356ee2afa79c
Waqas Shaikh <farazshaikh7837@gmail.com>
2023-12-11 15:19:36 +0500

    css styling

--- js/script.js
+++ js/script.js
@@ -3,6 +3,44 @@ document.addEventListener('DOMContentLoaded', function () {
   const navbarLinks = document.getElementById('navbar-links');
 
   mobileMenuButton.addEventListener('click', function () {
-    navbarLinks.classList.toggle('show');
+    document.addEventListener("DOMContentLoaded", function () {
+      // Your script code here
+    });
+  })
+  document.addEventListener("DOMContentLoaded", function () {
+    var Btn = document.querySelector(".btn--gsap");
+
+    var section1 = document.querySelector(".section-1");
+
+
+    section1.addEventListener("mousemove", function (event) {
+      console.log("ok");
+
+      gsap.to(Btn, {
+        x: event.x,
+        y: event.y
+      });
+    });
+  }); 
+  
+
+var Btn = document.querySelector(".btn--gsap");
+var section1 = document.querySelector(".section-1");
+
+section1.addEventListener("mouseenter", function (event) {
+
+  gsap.to(Btn, {
+    scale: 1,
+    opacity: 1
   });
+});
+section1.addEventListener("mouseleave", function (event) {
+
+  gsap.to(Btn, {
+    scale: 0,
+    opacity: 0
+  });
+});
+
+
 });
