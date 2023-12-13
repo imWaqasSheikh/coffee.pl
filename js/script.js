d091c7da5c4f50980990ce1d3a5053364fabbdcf
Waqas Shaikh <farazshaikh7837@gmail.com>
2023-12-13 18:20:01 +0500

    add buttons

--- js/script.js
+++ js/script.js
@@ -1,19 +1,62 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const mobileMenuButton = document.getElementById('mobile-menu');
-  const navbarLinks = document.getElementById('navbar-links');
+// var machines = document.querySelector(".display-machines");
+// var beans = document.querySelector(".display-beans");
+// var accosories = document.querySelector(".display-accosories");
+// var machinesBtn = document.querySelector(".machines-Btn");
+// var beansBtn = document.querySelector(".beans-Btn");
+// var accosoriesBtn = document.querySelector(".accosories-Btn");
 
-  mobileMenuButton.addEventListener('click', function () {
-    document.addEventListener("DOMContentLoaded", function () {
-      // Your script code here
-    });
-  })
-})
-  // mouse animated
+// beansBtn.addEventListener("click", function () {
+//   beans.style.display = "inline";
+//   machines.style.display = "none";
+//   accosories.style.display = "none";
+// });
+
+// machinesBtn.addEventListener("click", function () {
+//   beans.style.display = "none";
+//   machines.style.display = "inline";
+//   accosories.style.display = "none";
+// });
+
+// accosoriesBtn.addEventListener("click", function () {
+//   beans.style.display = "none";
+//   machines.style.display = "none";
+//   accosories.style.display = "inline";
+// });
+
+
+console.log("haha");
+
+var machines = document.querySelector(".display-machines");
+var beans = document.querySelector(".display-beans");
+var accosories = document.querySelector(".display-accosories");
+var machinesBtn = document.querySelector(".machines-Btn");
+var beansBtn = document.querySelector(".beans-Btn");
+var accosoriesBtn = document.querySelector(".accosories-Btn");
+
+beansBtn.addEventListener("click", function () {
+  beans.style.display = "inline";
+  machines.style.display = "none";
+  accosories.style.display = "none";
+});
+
+machinesBtn.addEventListener("click", function () {
+  beans.style.display = "none";
+  machines.style.display = "inline";
+  accosories.style.display = "none";
+});
+
+accosoriesBtn.addEventListener("click", function () {
+  beans.style.display = "none";
+  machines.style.display = "none";
+  accosories.style.display = "inline";
+});
+
+
+// mouse animated
 //   document.addEventListener("DOMContentLoaded", function () {
 //     var Btn = document.querySelector(".btn--gsap");
 
 //     var section1 = document.querySelector(".section-1");
-
 
 //     section1.addEventListener("mousemove", function (event) {
 //       console.log("ok");
@@ -23,8 +66,7 @@ document.addEventListener('DOMContentLoaded', function () {
 //         y: event.y
 //       });
 //     });
-//   }); 
-  
+//   });
 
 // var Btn = document.querySelector(".btn--gsap");
 // var section1 = document.querySelector(".section-1");
@@ -43,6 +85,5 @@ document.addEventListener('DOMContentLoaded', function () {
 //     opacity: 0
 //   });
 // });
-
 
 // })
